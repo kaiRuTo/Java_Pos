a3e5894b947d4257114107a6fc7e3cb7fa9b1117
Hoang Quan <14520724@gm.uit.edu.vn>
2020-07-20 15:00:10 +0700

    Coupon repository: find all usable coupon

--- src/main/java/com/mycompany/pos/repository/CouponRepository.java
+++ src/main/java/com/mycompany/pos/repository/CouponRepository.java
@@ -2,8 +2,14 @@ package com.mycompany.pos.repository;
 
 import com.mycompany.pos.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+    @Query(value = "SELECT * FROM coupon c WHERE c.is_used = true AND c.expiry_date > CURRENT_TIME")
+    List<Coupon> findAllAvailableCoupon();
 }
