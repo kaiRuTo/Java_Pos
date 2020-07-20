2b60051cb6d59a2fac9d9a809d6b3aaf415394da
Hoang Quan <14520724@gm.uit.edu.vn>
2020-07-20 14:27:57 +0700

    Add coupon repository

---
+++ src/main/java/com/mycompany/pos/repository/CouponRepository.java
@@ -0,0 +1,10 @@
+package com.mycompany.pos.repository;
+
+import com.mycompany.pos.entity.Coupon;
+import com.mycompany.pos.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+}
